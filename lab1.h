cc806dd2bd0ecd07b74239cc02289cfdd7e72515
Karina Malij <karinamalij0303@gmail.com>
2025-03-05 00:05:08 +0200

    update commit

--- lab1.h
+++ lab1.h
@@ -1,1 +1,11 @@
 #pragma once
+#ifndef LAB1_H
+#define LAB1_H
+
+#include <vector>
+#include <utility>
+using namespace std;
+vector<pair<int, int>> primeFactorization(int n);
+int eulerFunc(int n);
+
+#endif  
